d974eec79058ab8eaf8300643df140de559a3d54
Claire Clemot <claire.clemot@student.ecp.fr>
2018-03-21 12:19:16 +0100

    TimeZone fixed

--- timeout_backend/settings.py
+++ timeout_backend/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
